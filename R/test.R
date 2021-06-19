7ecbf3137b3e5c42d443fd7c2f58e51a73dbb891
Felipe Mattioni Maturana <felipemattioni92@gmail.com>
2021-06-19 17:35:10 +0200

    added excel call

--- R/test.R
+++ R/test.R
@@ -1,2 +1,8 @@
 #' @export
 return_iris <- function() jsonlite::toJSON(iris)
+
+#' @export
+return_iris_excel <- function() {
+  tmp <- readxl::read_excel(system.file("test.xlsx", package = "reprexhordes"))
+  jsonlite::toJSON(iris)
+}
